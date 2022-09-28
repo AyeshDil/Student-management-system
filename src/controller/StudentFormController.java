f2a6e18a810a12d85596530f1fa5c5933dc098c9
AyeshDil <dilshanwma@gmail.com>
2022-09-28 23:28:40 +0530

    Added hibernate.cfg.xml

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,2 +1,25 @@
-package controller;public class StudentFormController {
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class StudentFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public RadioButton rbtnMale;
+    public ToggleGroup Gender;
+    public RadioButton rbtnFemale;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colGender;
+    public TableColumn colOption;
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/entity/Student.java
+++ src/entity/Student.java
@@ -1,8 +1,62 @@
 package entity;
 
-public class Student {
-    String id;
-    String name;
-    String address;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Student {
+    @Id
+    private Long id;
+
+    @Column(name = "student_name", nullable = false)
+    private String name;
+
+    @Column(name = "student_address")
+    private String address;
+
+    @Column(name = "student_gender")
+    private String gender;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String address, String gender) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
